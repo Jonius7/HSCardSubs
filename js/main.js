ce5bdcbae8ceaa171627c56df89b011a8ddb1d84
Jason Ho <jason.ho@outlook.com.au>
2018-01-18 23:51:36 +1100

    Adding sliding animation for div

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     $("#showwild").hide();
     $("#link1").click(function(){
-        $("#showwild").show();
+        if ( $("#showwild").is(":hidden") ) {
+            $("#showwild").slideDown(400);
+        } else {
+            $("#showwild").slideUp(400);
+        }
+        //$("#showwild").slideDown(400);
     });
 });
