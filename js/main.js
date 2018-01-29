fecb178f7f2e52cc6c61f8c5bda5c28f8b046264
Jason Ho <jason.ho@outlook.com.au>
2018-01-29 12:51:57 +1100

    Add Firebase + changes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $(document).ready(function(){
         //$("#showwild").slideDown(400);
     });
 });
+
+var similar = [
+    {
+        name: "Arcanologist",
+        cardSet: "Journey to Un'goro",
+        rarity: "Common",
+        cost: 2, attack: 2, health: 3,
+        text: "<b>Battlecry:</b> Draw a <b>Secret</b> from your deck.",
+        playerClass: "Mage",
+        img: "http://media.services.zam.com/v1/media/byName/hs/cards/enus/UNG_020.png"
+    }
+]
+
+
+function rivercrocolisk($) {
+
+}
