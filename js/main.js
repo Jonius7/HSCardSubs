b7c954175d9a232e91f4432a4c1ddf5ba7effa39
Jason Ho <jason.ho@outlook.com.au>
2018-01-29 22:05:58 +1100

    testing a mix between static css and css generated by jQuery

--- js/main.js
+++ js/main.js
@@ -49,44 +49,44 @@ $(document).ready(function(){
 
     var raritytable = [
         {
-            name: "Free", color: "#cecece"
+            name: "Free", color: "#3c3c3c"
         },
         {
-            name: "Common", color: "#e59902"
+            name: "Common", color: "#f6f6f6"
         },
         {
-            name: "Rare", color: "#ef40a3"
+            name: "Rare", color: "#2570e5"
         },
         {
-            name: "Epic", color: "#c325ce"
+            name: "Epic", color: "#8f2aac"
         },
         {
-            name: "Legendary", color: "#707070"
+            name: "Legendary", color: "#d08d0b"
         }
     ]
 
     
     var seticons = [
         {
-            name: "Basic", code: "BASIC", color: "#cecece"
+            name: "Basic", code: "BASIC", color: "#CECECE"
         },
         {
-            name: "Classic", code: "CLAS", color: "#e59902"
+            name: "Classic", code: "CLAS", color: "#9E562C"
         },
         {
-            name: "Whispers of the Old Gods", code: "WOG", color: "#ef40a3"
+            name: "Whispers of the Old Gods", code: "WOG", color: "#CE6190"
         },
         {
             name: "One Night in Karazhan", code: "KARA", color: "#c325ce"
         },
         {
-            name: "Mean Streets of Gadgetzan", code: "MSG", color: "#707070"
+            name: "Mean Streets of Gadgetzan", code: "MSG", color: "#526057"
         },
         {
-            name: "Journey to Un'Goro", code: "UNG", color: "#63c645"
+            name: "Journey to Un'Goro", code: "UNG", color: "#A3D54F"
         },
         {
-            name: "Knights of the Frozen Throne", code: "KFT", color: "#3c82c4"
+            name: "Knights of the Frozen Throne", code: "KFT", color: "#49c1ce"
         },
         {
             name: "Kobolds & Catacombs", code: "KNC", color: "#ffb600"
@@ -94,17 +94,16 @@ $(document).ready(function(){
     ]
 
     function create_icons() {
-        var obj = $.grep(seticons, function(e){return e.name == "Classic";});
-        //alert(obj[0].code);
-        $(testicons)
-        .html('<div id="seticon">'+ obj[0].code + '</div>')
-        .css({
-            'background-color': obj[0].color,
-            'border': '1px solid black',
-            'width': '60px',
-            'height': '30px',
-            'font': 'Helvetica, Arial, sans-serif'
-        })
+        var order = ["Classic", "Journey to Un'Goro", "Knights of the Frozen Throne"];
+        $.each(order, function(i, value) {
+            var obj = $.grep(seticons, function(e){return e.name == value;});
+            $('#testicons').append('<div class="seticon">' + obj[0].code + '</div>');
+            $('#testicons > div:last').css( {
+                'background-color': obj[0].color
+             });
+        });
+        //var obj = $.grep(seticons, function(e){return e.name == "Classic";});
+        //alert(obj[0].code);       
     }
     
     function river_crocolisk() {
